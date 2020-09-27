8e3a19d93f1c862058ab01ec7dc311df2f772bf4
Hyeonjun <wns8363@gmail.com>
2020-09-27 16:12:05 +0900

    원거리 공격 구현, 총알 구현
    이동 방향에 따른 방향 조정 필요함

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,6 +10,9 @@ public class Weapon : MonoBehaviour
     public float rate;
     public BoxCollider2D meleeArea;
     public TrailRenderer trailEffect;
+    public Transform bulletPos;
+    public GameObject bullet;
+
     GameObject Arm;
     float ro_angle;
     public void Use() //플레이어가 무기를 사용
@@ -18,6 +21,11 @@ public class Weapon : MonoBehaviour
         {
             StopCoroutine("Swing");
             StartCoroutine("Swing");
+        }
+        else if(type == Type.Range)
+        {
+            StopCoroutine("Shot");
+            StartCoroutine("Shot");
         }
     }
     IEnumerator Swing()
@@ -32,9 +40,30 @@ public class Weapon : MonoBehaviour
         meleeArea.enabled = true;
         trailEffect.enabled = true;
         Arm.transform.parent.rotation = Quaternion.Euler(0, 0, ro_angle*6);
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.1f);
         meleeArea.enabled = false;
         Arm.transform.parent.rotation = Quaternion.Euler(0, 0, ro_angle*3);
+        yield return new WaitForSeconds(0.1f);
+        Arm.transform.parent.rotation = Quaternion.Euler(0, 0, 0);
+        trailEffect.enabled = false;
+    }
+    IEnumerator Shot()
+    {
+        
+        GameObject instantBullet = Instantiate(bullet, new Vector2(bulletPos.position.x, bulletPos.position.y), Quaternion.identity);
+        Rigidbody2D bulletRigid = instantBullet.GetComponent<Rigidbody2D>();
+        bulletRigid.velocity = bulletPos.right * 10;
+        Arm = gameObject;
+        if (gameObject.transform.parent.parent.localScale.x == -1)
+            ro_angle = 15;
+        else
+            ro_angle = -15;
+        Arm.transform.parent.rotation = Quaternion.Euler(0, 0, ro_angle * 1);
+        yield return new WaitForSeconds(0.1f);
+        trailEffect.enabled = true;
+        Arm.transform.parent.rotation = Quaternion.Euler(0, 0, ro_angle * 2);
+        yield return new WaitForSeconds(0.1f);
+        Arm.transform.parent.rotation = Quaternion.Euler(0, 0, ro_angle * 1);
         yield return new WaitForSeconds(0.1f);
         Arm.transform.parent.rotation = Quaternion.Euler(0, 0, 0);
         trailEffect.enabled = false;

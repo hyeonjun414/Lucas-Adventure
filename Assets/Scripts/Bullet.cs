c25abd9f18e757c2eae98e73b18930e2af411fad
Hyeonjun <wns8363@gmail.com>
2020-10-01 23:44:24 +0900

    몬스터 피격 구현
    근접, 원거리 공격에 대한 피격구현
    적 넉백구현 미완성

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,11 +6,19 @@ public class Bullet : MonoBehaviour
 {
     public int damage;
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy" || other.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+    /*
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag != "Player")
         {
             Destroy(gameObject, 0.1f);
         }
-    }
+    }*/
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+
+    Rigidbody2D rigid;
+    BoxCollider2D boxCollider;
+    Material mat;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        mat = GetComponent<SpriteRenderer>().material;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Melee")
+        {
+            Weapon weapon = other.GetComponent<Weapon>();
+            curHealth -= weapon.damage;
+            Vector2 reactVec = transform.position - other.transform.position;
+            StartCoroutine(OnDamage(reactVec));
+            Debug.Log("Melee : " + curHealth);
+        }
+        else if (other.tag == "Bullet")
+        {
+            Bullet bullet = other.GetComponent<Bullet>();
+            curHealth -= bullet.damage;
+            Vector2 reactVec = transform.position - other.transform.position;
+            StartCoroutine(OnDamage(reactVec));
+            Debug.Log("Range : " + curHealth);
+        }
+    }
+
+    IEnumerator OnDamage(Vector2 reactVec)
+    {
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if(curHealth > 0)
+        {
+            mat.color = Color.white;
+            reactVec = reactVec.normalized;
+            reactVec += Vector2.right;
+            rigid.AddForce(reactVec * 5, ForceMode2D.Impulse);
+        }
+        else
+        {
+            mat.color = Color.gray;
+            gameObject.layer = 9;
+
+            
+            Destroy(gameObject, 2);
+        }
+
+    }
+
+}

15fbf76e54ba431df522a4a3dcd2baaab9d10e87
ktgokr <38237922+ktgokr@users.noreply.github.com>
2020-11-07 23:28:29 +0900

    깜빡임이 한번으로 끝남
    fade.FadeOut(0.1f);
            yield return new WaitForSeconds(0.2f);
            player.curMapName = transferMapName;
    
            SceneManager.LoadScene(transferMapName);
            fade.FadeIn(0.1f);
    
    => 숫자를 조절해서 한번 빠르게 깜박이여서 넘어감. 다만 조금 느리게 넘어가고 싶은데 그점은 아직 풀어야 할 숙제

---
+++ Assets/Scripts/Fade_Manager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Fade_Manager : MonoBehaviour
+{
+
+    public SpriteRenderer white; 
+    public SpriteRenderer black;
+    private Color color;
+    private WaitForSeconds waitTime = new WaitForSeconds(0.01f);
+
+    public void FadeOut(float _speed )
+    {
+        StartCoroutine(FadeOutCoroutine(_speed));
+    }
+    IEnumerator FadeOutCoroutine(float _speed)
+    {
+
+        color = black.color;
+
+        while (color.a < 1f)
+        {
+            color.a += _speed;
+            black.color = color;
+            // yield return new WaitForSeconds(0.01f);
+            yield return waitTime;
+        }
+
+    }
+    
+    public void FadeIn(float _speed )
+    {
+        StartCoroutine(FadeInCoroutine(_speed));
+    }
+    IEnumerator FadeInCoroutine(float _speed)
+    {
+
+        color = black.color;
+
+        while (color.a > 0f)
+        {
+            color.a -= _speed;
+            black.color = color;
+            //yield return new WaitForSeconds(0.01f);
+            yield return waitTime;
+
+        }
+
+    }
+
+    
+
+
+
+    public void FlashOut(float _speed)
+    {
+        StartCoroutine(FlashOutCoroutine(_speed));
+    }
+    IEnumerator FlashOutCoroutine(float _speed)
+    {
+
+        color = white.color;
+
+        while (color.a < 1f)
+        {
+            color.a += _speed;
+            white.color = color;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+    }
+
+    public void FlashIn(float _speed)
+    {
+        StartCoroutine(FlashInCoroutine(_speed));
+    }
+    IEnumerator FlashInCoroutine(float _speed)
+    {
+
+        color = white.color;
+
+        while (color.a > 0f)
+        {
+            color.a -= _speed;
+            white.color = color;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+    }
+
+}
--- Assets/Scripts/MapTransform.cs
+++ Assets/Scripts/MapTransform.cs
@@ -7,19 +7,31 @@ public class MapTransform : MonoBehaviour
     public string transferMapName; //이동맵
     public Player player;
     public GameObject startingPoint;
+    public Fade_Manager fade;
+    //private OrderManager theOrder;
     // Update is called once per frame
     void Start()
     {
         player = FindObjectOfType<Player>();
+        fade = FindObjectOfType<Fade_Manager>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player")
         {
-            player.curMapName = transferMapName;
-            SceneManager.LoadScene(transferMapName);
+            StartCoroutine(TransferCoroutine());
         }
+    }
+
+    IEnumerator TransferCoroutine()
+    {
+        fade.FadeOut(0.1f);
+        yield return new WaitForSeconds(0.2f);
+        player.curMapName = transferMapName;
+
+        SceneManager.LoadScene(transferMapName);
+        fade.FadeIn(0.1f);
     }
 
 }

5ecf2e76a8d77b0309c6a3b1f90334b9b38a9b86
ktgokr <38237922+ktgokr@users.noreply.github.com>
2020-11-14 22:32:10 +0900

    stage clear condition done
    몬스터 소환 및 다 죽으면 게이트가 오픈되는 것은 성공. 그러나 enemycount가 -1이 될것이 -100이 됨 그부분만 해결이 안됨.

--- Assets/Scripts/Gate_set.cs
+++ Assets/Scripts/Gate_set.cs
@@ -6,18 +6,50 @@ public class Gate_set : MonoBehaviour
 {
     [SerializeField] private Gate_Open gate;
 
+    public Transform[] spawnpoints; //몬스터 위치
+    public GameObject enemy;    //몬스터 객체
+
+    public int Max;//몬스터 최대 생성 가능한 수
+    public int EnemyCnt; //몬스터 카운트
+    public bool[] isSpawn;
+
+    public static Gate_set _instance; //Enemy에서 Gate_set에 접근하기 위한 정적변수
+
+
+    private void Start()
+    {
+        isSpawn = new bool[spawnpoints.Length];
+        for (int i = 0; i < spawnpoints.Length; i++)
+        {
+            isSpawn[i] = false;
+        }
+        _instance = this;
+    }
+
+
     // Update is called once per frame
     private void Update()
     {
+        if (EnemyCnt < Max) //Max보다 작으면 생성
+        { 
+            int x = Random.Range(0, spawnpoints.Length); //0~max수까지
+            if(!isSpawn[x]) //해당위치가 false이면 그 위치에 생성
+            SpawnEnemy(x); // 몬스터 생성
+        }
 
-        if (Input.GetKeyDown(KeyCode.F)) {
+        if (EnemyCnt<=-495) //EnemyCnt값이 -495이하이면 게이트가 열린다.
+        {
             gate.OpenGate();
         }
-        /*
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            gate.CloseGate();
-        }
-        */
+
+       
     }
+
+    public void SpawnEnemy(int randNum) //몬스터 생성함수
+    {
+        EnemyCnt++; //생성될때마다 몬스터 수 카운트
+        Instantiate(enemy, spawnpoints[randNum]);   //몬스터소환, 랜덤위치 생성
+        isSpawn[randNum] = true; //해당위치가 true이면 그 위치에 자동생성 x
+    }
+
 }

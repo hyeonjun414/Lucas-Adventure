e274cd34f497e064ef7fdf9dbfeddb1e716c8f53
ktgokr <38237922+ktgokr@users.noreply.github.com>
2020-11-30 20:02:45 +0900

    binary

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable] 
+public class SaveData {
+
+    public float Speed; //기본 이동속도
+
+    public int damage;
+    public int armor;
+
+    public string curMapName;
+    public int coin;  //가지고 있는 코인
+    public int health;// 현재 체력
+    public float exp;   // 현재 경험치
+    public int level;
+
+    public float x;
+    public float y;
+
+    public SaveData(Player player)
+    {
+        Speed = player.Speed;
+        health = player.health; //hp
+        level = player.level; //level
+        damage = player.damage; // damage
+        armor = player.armor; //아머
+        coin = player.coin; //코인
+        exp = player.exp; //경험치
+        curMapName = player.curMapName; //맵
+      
+        x = player.transform.position.x;  //x좌표
+        y = player.transform.position.y;  //y좌표
+    }
+
+
+}
---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveManager {
+
+    public static void Save(Player player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        SaveData data = new SaveData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static SaveData Load()
+    {
+
+        string path = Application.persistentDataPath + "/player.fun";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            SaveData data =  formatter.Deserialize(stream) as SaveData ;
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in" + path);
+            
+            return null;
+        }
+       
+        /*
+        try
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Path.Combine(Application.dataPath, "LD.bin");
+            FileStream stream = File.OpenRead(path);
+            SaveData data = (SaveData)formatter.Deserialize(stream);
+            stream.Close();
+            return data;
+        }
+
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+            return default;
+        }
+        */
+    }
+
+}
+ 
